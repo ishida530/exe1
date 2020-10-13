6b7f3283377aab4afadd0f77bf30964f86f80c00
ishida530 <ishida530@gmail.com>
2020-10-13 14:44:51 +0200

    exe1

---
+++ js.js
@@ -0,0 +1,19 @@
+
+const div = document.querySelector("#wynik");
+let number = 0;
+div.style.backgroundColor = "red"
+
+
+
+let kodHTML='';
+        for(let i=0;i<=9;i++)
+        
+        {
+            kodHTML = `${kodHTML} <table textC>${i}<tr>   ${i * i}`;
+            document.quer
+         
+
+            
+            
+        }
+        wynik.innerHTML=kodHTML;
